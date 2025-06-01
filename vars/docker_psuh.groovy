ca1f274a60b7a530918847e39082681d6d692aea
Vicky kumawat <vickykumawat766@gmail.com>
2025-06-01 11:24:48 +0530

    Create docker_psuh.groovy

---
+++ vars/docker_psuh.groovy
@@ -0,0 +1,8 @@
+def call( String dockerHubUser, String ImageName, String ImageTag) {
+
+   echo "This is Tpushing the code to Dockerhub"
+    withCredentials([usernamePassword(credentialsId:"dockerhublogin",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    sh "docker push ${dockerHubUser}/{ImageName}:${ImageTag}"
+                }  
+}
